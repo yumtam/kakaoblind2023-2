030843664b922230f3fe52b0542e64c02cd335b8
yumtam kim <yumtamking@gmail.com>
2022-10-26 23:38:58 +0900

    Add params.py

---
+++ task/params.py
@@ -0,0 +1,15 @@
+task_1_params = {
+    "task_no": 1,
+    "max_turns": 200,
+    "hotel_width": 20,
+    "hotel_height": 3,
+}
+
+task_2_params = {
+    "task_no": 2,
+    "max_turns": 1000,
+    "hotel_width": 200,
+    "hotel_height": 10,
+}
+
+task_params = [task_1_params, task_2_params]
